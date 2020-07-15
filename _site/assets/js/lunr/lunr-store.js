8f09983b9ef20f13484f22ac16eef8dd3e0f176f
Saket Khandelwal <saketdecade17@gmail.com>
2020-07-15 14:21:25 +1000

    Added first blog post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Welcome to  Saket.log",
+        "excerpt":"Namaste! Sawasdee! Anyong! Nihao! Ohayo! I have been wanting to develop a personal blog for a while now. During my winter vacation, my friend (Abhinav) introduced me to jekyll and the minimal mistakes theme. It was the perfect time to explore markdown driven blogging as I had time on my...","categories": ["blog"],
+        "tags": ["blog","lifestyle"],
+        "url": "http://0.0.0.0:4000/blog/blog-info/",
+        "teaser": null
+      }]
