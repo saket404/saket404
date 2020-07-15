ac3ad252d28b244fc94bc48d095203ef92e592b2
Saket Khandelwal <saketdecade17@gmail.com>
2020-07-15 19:44:03 +1000

    Added disqus to site posts.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Welcome to  Saket.log",
         "excerpt":"Namaste! Sawasdee! Anyong! Nihao! Ohayo! I have been wanting to develop a personal blog for a while now. During my winter vacation, my friend (Abhinav) introduced me to jekyll and the minimal mistakes theme. It was the perfect time to explore markdown driven blogging as I had time on my...","categories": ["blog"],
         "tags": ["blog","lifestyle"],
-        "url": "http://0.0.0.0:4000/blog/blog-info/",
+        "url": "http://0.0.0.0:4000/blog/welcome/",
         "teaser": null
       }]
